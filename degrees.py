a46b9347386f3c474b5c506393463e69e1d67c87
serignebidev <serignebidev@gmail.com>
2023-12-13 07:23:20 +0000

    stable commit

--- degrees.py
+++ degrees.py
@@ -111,7 +111,6 @@ def shortest_path(source, target):
                 shortestPath.insert(0, [tempNode.action, tempNode.state])
                 tempNode = tempNode.parent
 
-            # print(movies)
             return shortestPath
         else:
             for movieId in people[removedNode.state]['movies']:
